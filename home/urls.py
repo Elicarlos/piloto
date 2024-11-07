a6ebb04c9d0d25cf6028ae4bec01b99629b92a2b
Elicarlos <elicarlosantos_@hotmail.com>
2024-11-06 21:40:23 -0300

    feito ajuste de template

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.index, name="home"),
     path('contato/', views.contato, name="contato"),
     path('ajuda/', views.ajuda, name="ajuda"),
-    path('sobre/', views.sobre, name="sobre")
+    path('sobre/', views.sobre, name="sobre"),
+    path('perfil/<str:nome>/', views.perfil, name="perfil"),
+    path('item/<int:id>/', views.exibir_item, name="exibir_item"),
+    path('diasemana/<int:dia>/', views.diasemana, name="diasemana"),
+    path('dashboard/', views.dashboard, name="dashboard"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render
 
 # Create your views here.
@@ -14,5 +15,63 @@ def ajuda(request):
 
 def contato(request):
     return render(request, 'contato.html')
+
+def perfil(request, nome):
+    return render(request, 'perfil.html', {"nome": nome})
+    
+def diasemana(request, dia):
+    
+    try:
+        dia = int(dia)
+    
+    except ValueError:
+        return HttpResponseBadRequest("Insira um valor numérico para o dia.")
+    
+  
+    mensagem = ""
+    
+    if dia > 7 :
+        mensagem ="Digite um numero menor que 7"
+        
+    elif dia == 0:
+        mensagem = "Insira um numero de 1  a 7"
+        
+    if dia == 1:
+        mensagem = "domingo"
+    
+    if dia ==2:
+        mensagem = "segunda-feira"
+        
+    if dia == 3:
+        mensagem = "terça-feira"
+        
+    if dia == 4:
+        mensagem = "quarta-feira"
+        
+    if dia == 5:
+        mensagem = "quinta-feira"
+        
+    if dia == 6:
+        mensagem = "sexta-feira"
+        
+    if dia == 7:
+        mensagem = "sexta-feira"
+        
+   
+    
+    
+    context = {
+        "mensagem": mensagem
+    }
+    
+    return render(request, 'dia-semana.html', context)
+
+
+def exibir_item(request, id):
+    return render(request, 'exibir_item.html', {"id": id})
+
+
+def dashboard(request):
+    return render(request, 'home/dashboard.html')
 
 
