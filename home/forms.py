045069ef699af8d84ce951e18a46feb2c7a9d9d3
Elicarlos <elicarlosantos_@hotmail.com>
2024-11-15 17:07:29 -0300

    feito ajustes de home

--- home/forms.py
+++ home/forms.py
@@ -1,11 +1,32 @@
 from django import forms
 
 class ContatoForm(forms.Form):
-    nome = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    mensagem = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    nome = forms.CharField(
+        label="Nome Completo",
+        max_length=250,
+       
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Insira o seu nome' })
+        )
+    email = forms.EmailField(
+        label="Email",
+        max_length=250,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'insira o seu email'})
+        )
+    mensagem = forms.CharField(
+        label="Mensagem",
+        max_length=250,
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Insira sua mensagem'})
+    )
     
     
 class ProdutoForm(forms.Form):
-    nome = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
-    preco = forms.DecimalField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    nome = forms.CharField(
+        label="Nome",
+        max_length=250,
+        widget=forms.TextInput(attrs={"class": "form-control", 'placeholder': 'Insira o nome do produto...'})
+    )
+    preco = forms.DecimalField(
+        label="Preço",
+        max_digits=10,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Preço'})
+    )
